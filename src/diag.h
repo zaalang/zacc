d88206a6f85b66d23f6bd626bd66b45881440c4f
pniekamp <pniekamp@gmail.com>
2022-01-02 18:19:44 +1100

    headers

--- src/diag.h
+++ src/diag.h
@@ -1,7 +1,7 @@
 //
 // diag.h
 //
-// Copyright (C) 2020 Peter Niekamp. All rights reserved.
+// Copyright (C) 2020-2022 Peter Niekamp. All rights reserved.
 //
 // This file is part of zaalang, which is BSD-2-Clause licensed.
 // See http://opensource.org/licenses/BSD-2-Clause
